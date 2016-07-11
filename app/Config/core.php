fd48dfa158167b73b43e6577ac563bebb6b6d5b2
Gaurav Ulhe <gauravu@sanisoft.com>
2016-07-11 10:33:11 +0530

    no changes made this time.

--- app/Config/core.php
+++ app/Config/core.php
@@ -226,7 +226,7 @@
  * A random string used in security hashing methods.
  */
 	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2gufgfVoUubWwvniR2G0FgaC9mi');
-
+										
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
