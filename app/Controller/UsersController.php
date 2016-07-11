b1ec2e07504cce32e4ba45019fefc403f6e94b09
Gaurav Ulhe <gauravu@sanisoft.com>
2016-07-11 13:03:03 +0530

    added add method in users controller

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -5,14 +5,10 @@
 	class UsersController extends AppController
 	{
 		public function index(){
-<<<<<<< HEAD
-=======
-
 		}
 
 		public function add(){
->>>>>>> a133a84bf6830be4a8a0adf9a55cbbbc6a953c5b
-			
+
 		}
 	}
 ?>
